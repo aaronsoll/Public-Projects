4c79bf5beeb2a1ccdc1c1f752f6606ef5440f3bf
Aaron Soll <77807104+aaronsoll@users.noreply.github.com>
2021-08-27 15:01:15 -0700

    Delete 2D Random World Generator directory

--- 2D Random World Generator/byow/Core/Experiment.java
+++
@@ -1,8 +0,0 @@
-package byow.Core;
-
-public class Experiment {
-
-    public static void main(String[] args) {
-
-    }
-}
--- 2D Random World Generator/byow/Core/Probabilities.java
+++
@@ -1,7 +0,0 @@
-package byow.Core;
-
-public class Probabilities {
-    public static final int TURN = 2;
-    public static final double ROOM = 12;
-    public static final double BRANCH = 3;
-}
